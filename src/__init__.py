c8a5c9285650a3c06cfe76661f14e2bab85b85c5
cristianreynams <cristian.reynams@gmail.com>
2025-12-11 14:09:30 -0800

    Initialize package with version and author info

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+# Este archivo hace que Python trate el directorio como un paquete
+from .pipeline import NYC_Airbnb_ETL
+
+__version__ = "1.0.0"
+__author__ = "cristianreynams"
+__all__ = ['NYC_Airbnb_ETL']
